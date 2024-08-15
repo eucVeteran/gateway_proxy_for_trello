e15bbebaecef998f4cf8bcae98f738e411f52951
Azizbek Toshpulatov <xtoshpul@fi.muni.cz>
2024-08-15 13:22:00 +0200

    Init

---
+++ src/main/java/org/trello/gatewayproxy/gateway_proxy_for_trello/GatewayProxyForTrelloApplication.java
@@ -0,0 +1,13 @@
+package org.trello.gatewayproxy.gateway_proxy_for_trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayProxyForTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayProxyForTrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/trello/gatewayproxy/gateway_proxy_for_trello/GatewayProxyForTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trello.gatewayproxy.gateway_proxy_for_trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayProxyForTrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
